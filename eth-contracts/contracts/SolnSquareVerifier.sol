25dce1068d0b30f2dd646866093b1cf883186718
Nikos <nikoskordis@gmail.com>
2021-04-17 18:21:30 +0200

    feat: Can be mint a ERC721 token

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,38 +1,76 @@
 pragma solidity >=0.4.21 <0.6.0;
+import "./verifier.sol";
+import "./ERC721Mintable.sol";
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+contract VerifierZ is Verifier {
 
+}
 
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is CapstoneERC721Token {
 
-
+    VerifierZ public verifier;
 
 // TODO define a solutions struct that can hold an index & an address
-
+    struct Solution {
+        uint256 index;
+        address addressSol;
+    }
 
 // TODO define an array of the above struct
-
+ Solution[] private solutions;
 
 // TODO define a mapping to store unique solutions submitted
-
+ mapping (bytes32 => address) private uniqueSolutionsSubmitted;
 
 
 // TODO Create an event to emit when a solution is added
+event SolutionAdded(uint256 index, address verifierAddress);
 
+    constructor (string memory name, string memory symbol, address verifierAddress)  CapstoneERC721Token(name, symbol) public
+    {
+        verifier = VerifierZ(verifierAddress);
+    }
 
 
 // TODO Create a function to add the solutions to the array and emit the event
+function addSolution(uint256 index, address address_, bytes32 key) public {
+        
+        Solution memory solution = Solution ({
+                                                index: index,
+                                                addressSol: address_
+                                            });
 
+        solutions.push(solution);
+        uniqueSolutionsSubmitted[key] = address_;
+        emit SolutionAdded(index, address_);
+
+    }
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
+function mintNFT(address to, uint256 index,  uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory inputs) public {
+        
+        // GenerateKey from solution inputs
+        bytes32 solutionHash = keccak256(abi.encodePacked(a, b, c, inputs));
+        
+        require(verifier.verifyTx(a, b, c, inputs ), 'Solution is not correct');
+        addSolution(index, to, solutionHash);
 
+        super.mint(to, index);
+
+    }
+
+    function generateKey(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory inputs)  public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(a, b, c, inputs));
+    }
   
 
-
+}
 
 
 
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,43 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const Verifier = artifacts.require('Verifier')
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier')
+const proofZokrates = require('../../zokrates/code/square/proof.json');
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('TestSolnSquareVerifier', accounts => {
+    
+    const owner = accounts[0]
+    const account_two = accounts[1]
+    const name = "ERC721Token"
+    const symbol = "ERC721T"
+    let tokenId = 1
+
+    beforeEach(async function() {
+
+        this.verifier = await Verifier.new({from: owner})
+        this.contract = await SolnSquareVerifier.new(name, symbol, this.verifier.address, {from: owner})
+    })
+    // Test if a new solution can be added for contract - SolnSquareVerifier
+    it('Should add a new solution', async function (){
+
+        // Generate a solution key
+        let key = await this.contract.generateKey.call(proofZokrates.proof.a,  proofZokrates.proof.b,  proofZokrates.proof.c, proofZokrates.inputs)
+        let tx = await this.contract.addSolution(tokenId, owner, key)
+        assert.equal(tx.logs[0].event, "SolutionAdded", 'Solution could not be added')
+
+    })
+
+    // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+    it('Should mint a new ERC721 token', async function (){
+
+        let totalSupplyBefore = (await this.contract.totalSupply.call()).toNumber()
+        await this.contract.mintNFT(account_two, tokenId, proofZokrates.proof.a,  proofZokrates.proof.b,  proofZokrates.proof.c, proofZokrates.inputs, {from: owner})
+        let totalSupplyAfter = (await this.contract.totalSupply.call()).toNumber()
+        let tokenBalance = (await this.contract.balanceOf.call(account_two)).toNumber()
+        let tokenURI = await this.contract.tokenURI.call(tokenId)
+
+        assert.equal(totalSupplyAfter, totalSupplyBefore + 1, ' Incorrect total supply')
+        assert.equal(tokenBalance,  1, 'Incorrect balance')
+        assert.equal(tokenURI, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1", "Token URI is invalid")
+
+    })
+
+})
